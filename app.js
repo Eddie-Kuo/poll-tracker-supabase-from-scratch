fa93ff34e0eefc59d33d730f3fcab89d23b190d0
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-08-12 13:19:54 -0500

    on submit, user will see current polls updated with input

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 
 
+// DOM elements
 const inputForm = document.querySelector('.input-section');
+const currentQuestionEl = document.querySelector('.current-question');
+const currentOptOne = document.querySelector('.current-opt-one');
+const currentOptTwo = document.querySelector('.current-opt-two');
+const optOnePlus = document.querySelector('.opt-one-plus');
+const optOneMinus = document.querySelector('.opt-one-minus');
+const optTwoPlus = document.querySelector('.opt-two-plus');
+const optTwoMinus = document.querySelector('.opt-two-minus');
 
+
+// state
 let userQuestion = '';
 let optionOne = '';
 let optionTwo = '';
 
+
+// form event listener
 inputForm.addEventListener('submit', (event) => {
 
     event.preventDefault();
@@ -15,7 +27,9 @@ inputForm.addEventListener('submit', (event) => {
     optionOne = data.get('option-one');
     optionTwo = data.get('option-two');
 
-    inputForm.reset();
-    console.log(userQuestion, optionOne, optionTwo);
+    currentQuestionEl.textContent = userQuestion;
+    currentOptOne.textContent = optionOne;
+    currentOptTwo.textContent = optionTwo;
 
+    inputForm.reset();
 });
